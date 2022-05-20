d298c73dca6276c9b6d614f0920da6fb3351c3ce
AspectWaltz <aspectwaltz@gmail.com>
2022-05-19 19:57:12 -0700

    adding camera scripts

--- scenes/Camera.gd
+++ scenes/Camera.gd
@@ -1,16 +1,61 @@
 extends Camera
 
+## SYSTEMCAMERAVARS
+# Look Settings
+var Pitch = 1;
+var PitchRange = 90;
+var Angle = 90;
+var Zoom = 1;
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Mouse position (for html5 & mobile)
+var mousePos = get_viewport().get_mouse_position();
+var mLockP = true;
 
+# Effects
+var Shake = 0;
 
-# Called when the node enters the scene tree for the first time.
+# Clamps
+var PitchRangePosition = 45;
+var p = clamp(PitchRange, 1, 89);
+var PitchClamp = Vector2(PitchRangePosition - p*.5, PitchRangePosition + p*.5);
+var ZoomClamp  = Vector2(0.25, 5);
+
+# Enablers
+var enable_pitch = true;
+var enable_angle = true;
+
+# Interpolate settings
+var pTo=Pitch;
+var aTo=Angle;
+var zTo=Zoom;
+
+# Directional
+var Forward = 0;
+var Backward = 0;
+var Right = 0;
+var Left = 0;
+
+# Gamepad device
+var Device = 0;
+
+# Sensitivity Settings
+var Sens = {
+	"Mouse"		: Vector2(.05, .05),
+	"Joystick"	: Vector2(1, 2)
+}
+
 func _ready():
-	pass # Replace with function body.
+	camera_initialize()
 
+func _process(delta):
+	camera_update()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func camera_initialize():
+	pass
+
+func camera_update():
+	pass
+	
+func shake_random():
+	pass
+	
